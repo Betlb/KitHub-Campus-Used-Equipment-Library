6c2482f5950c5b30671cd5f4f0157f11731851cf
Mehmet Karatekin <krtknmm38@zoho.com>
2025-07-23 18:41:14 +0300

    chore(assignment3): added some styling

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from flask import Flask
-from src.borrow.borrow_controller import borrow_bp
+from flask import Flask, render_template
 from src.borrow.db import db
+from src.borrow.borrow_controller import borrow_bp
 
 app = Flask(__name__)
-app.secret_key = 'helloWorld'
+app.secret_key = 'KitHub'
 
-# SQLite config for local
+# Config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kithub.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
-# Register Blueprint
 app.register_blueprint(borrow_bp)
 
-# Home route
 @app.route('/')
 def home():
-    return "<h1>Welcome to KitHub</h1><p><a href='/catalog'>View Equipment</a></p>"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
